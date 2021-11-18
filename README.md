f4fe28d48d712cfd08b84fb999a421266db996ed
Asus <dhanashreehakke7@gmail.com>
2021-11-18 17:24:15 +0530

    Added: Welcome message is added

---
+++ README.md
@@ -0,0 +1,1 @@
+AddressBookDay_24
---
+++ src/main/java/com/blp/addressbook/AddressBook.java
@@ -0,0 +1,8 @@
+package com.blp.addressbook;
+
+public class AddressBook {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to AddressBook using streams Program");
+    }
+}
